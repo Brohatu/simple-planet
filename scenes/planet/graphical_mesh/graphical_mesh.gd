27abf1719d28e7ffca0ee080233b2a9c7a967f38
Mitchell L <mitchell.lowe.21@gmail.com>
2025-12-02 19:01:35 +1100

    Update graphical_mesh.gd

--- scenes/planet/graphical_mesh/graphical_mesh.gd
+++ scenes/planet/graphical_mesh/graphical_mesh.gd
@@ -3,30 +3,22 @@ class_name GraphicalMesh extends MeshInstance3D
 
 
 var geometry_mesh:GeometryMesh
-var vertex_colours:Array[Color]
+var vertex_colours:PackedColorArray
 
 
-func _init() -> void:
+func initialise(g_mesh:GeometryMesh) -> void:
 	mesh = ArrayMesh.new()
-	#geometry_mesh = g_mesh
+	geometry_mesh = g_mesh
 	
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func create_mesh():
 	var vertices:PackedVector3Array = geometry_mesh.vertices
-	var face_indices:Array[int] = []
+	var face_indices:PackedInt32Array = []
 	face_indices.resize(geometry_mesh.faces.size()*3)
-	for i in range(geometry_mesh.face.size()):
+	for i in range(geometry_mesh.faces.size()):
 		face_indices[i*3+0] = geometry_mesh.faces[i][0]
 		face_indices[i*3+1] = geometry_mesh.faces[i][1]
 		face_indices[i*3+2] = geometry_mesh.faces[i][2]
@@ -50,13 +42,12 @@ func determine_vertex_colours():
 		vertex_colours[i] = Color.from_rgba8(temp_col.r8, temp_col.g8, temp_col.b8, temp_col.a8)
 
 
-func update_mesh_attributes(vertices:Array[Vector3], face_indices:Array[int]):
-	var mesh_arrays:Array[Array] = []
+func update_mesh_attributes(vertices:PackedVector3Array, face_indices:PackedInt32Array):
+	var mesh_arrays:Array = []
 	mesh_arrays.resize(Mesh.ARRAY_MAX)
 	mesh_arrays[Mesh.ARRAY_VERTEX] = vertices
 	mesh_arrays[Mesh.ARRAY_INDEX] = face_indices
 	mesh_arrays[Mesh.ARRAY_COLOR] = vertex_colours
 	
-	var m:ArrayMesh = ArrayMesh.new()
-	m.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,mesh_arrays)
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,mesh_arrays)
 	
