95d1f23f1cb465d191e049d456c7f8c13abd8e40
Mitchell L <mitchell.lowe.21@gmail.com>
2025-12-05 14:56:56 +1100

    Update graphical_mesh.gd

--- scenes/planet/graphical_mesh/graphical_mesh.gd
+++ scenes/planet/graphical_mesh/graphical_mesh.gd
@@ -14,12 +14,17 @@ func initialise(g_mesh:GeometryMesh) -> void:
 func create_mesh():
 	var vertices:PackedVector3Array = geometry_mesh.vertices
 	var face_indices:PackedInt32Array = []
-	var normals:PackedVector3Array = geometry_mesh.mesh.surface_get_arrays(0)[Mesh.ARRAY_NORMAL]
+	#var normals:PackedVector3Array = geometry_mesh.mesh.surface_get_arrays(0)[Mesh.ARRAY_NORMAL]
+	var normals:PackedVector3Array = vertices.duplicate()
+	
 	face_indices.resize(geometry_mesh.faces.size()*3)
 	for i in range(geometry_mesh.faces.size()):
 		face_indices[i*3+0] = geometry_mesh.faces[i][0]
 		face_indices[i*3+1] = geometry_mesh.faces[i][1]
 		face_indices[i*3+2] = geometry_mesh.faces[i][2]
+	
+	for v in normals:
+		v.normalized()
 	
 	vertex_colours = []
 	vertex_colours.resize(geometry_mesh.get_number_of_vertices())
@@ -27,7 +32,7 @@ func create_mesh():
 	determine_vertex_colours()
 	
 	update_mesh_attributes(vertices, face_indices, normals)
-	
+
 
 
 func determine_vertex_colours():
@@ -47,6 +52,6 @@ func update_mesh_attributes(vertices:PackedVector3Array, face_indices:PackedInt3
 	mesh_arrays[Mesh.ARRAY_VERTEX] = vertices
 	mesh_arrays[Mesh.ARRAY_INDEX] = face_indices
 	mesh_arrays[Mesh.ARRAY_COLOR] = vertex_colours
-	#mesh_arrays[Mesh.ARRAY_NORMAL] = normals
+	mesh_arrays[Mesh.ARRAY_NORMAL] = normals
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,mesh_arrays)
 	
