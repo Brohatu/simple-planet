468796d5eb34a9646ddf02fa610761090fcfcf3d
Mitchell L <mitchell.lowe.21@gmail.com>
2025-12-12 16:18:06 +1100

    Update topography.gd

--- scenes/tile/topography.gd
+++ scenes/tile/topography.gd
@@ -6,6 +6,12 @@ class_name Topography extends Resource
 @export var stress:float
 
 
-static func initialise_topography(tiles:Array[Tile]):
+static func initialise_topography(tiles:Array[Tile],data:PlanetData,mesh:GeometryMesh):
 	for t:Tile in tiles:
 		t.topography = new()
+		t.topography.set_topography(data.surface_noise,mesh.polygons[t.index].get_centre_vertex())
+
+
+
+func set_topography(noise:FastNoiseLite,v:Vector3):
+	bedrock = noise.get_noise_3dv(v)
