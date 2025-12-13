24cebb59026a0ac3d62afa411113655cfd697f18
Mitchell L <mitchell.lowe.21@gmail.com>
2025-12-13 14:33:31 +1100

    Update topography.gd

--- scenes/tile/topography.gd
+++ scenes/tile/topography.gd
@@ -12,6 +12,15 @@ static func initialise_topography(tiles:Array[Tile],data:PlanetData,mesh:Geometr
 		t.topography.set_topography(data.surface_noise,mesh.polygons[t.index].get_centre_vertex())
 
 
+static func generate_topography_colours(tiles:Array[Tile]) -> PackedColorArray:
+	var top_cols:PackedColorArray = []
+	top_cols.resize(tiles.size())
+	top_cols.fill(Color.WHITE)
+	for ti in range(tiles.size):
+		top_cols[ti] *= tiles[ti].get_attribute("altitude")
+	
+	return top_cols
+
 
 func set_topography(noise:FastNoiseLite,v:Vector3):
 	bedrock = noise.get_noise_3dv(v)
